1e390a452636c1c0456ba2f42c2e810820f3859b
Isarakyun <119465506+Isarakyun@users.noreply.github.com>
2024-07-12 16:54:51 +0800

    dark mode added in results.html

--- web/models.py
+++ web/models.py
@@ -12,17 +12,24 @@ class YoutubeUrl(db.Model):
 class Comments(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     comment = db.Column(db.String(50000))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     url_id = db.Column(db.Integer, db.ForeignKey('youtube_url.id'))
+    summarizedComments = db.relationship('SummarizedComments')
+    labeledComments = db.relationship('LabeledComments')
 
 class SummarizedComments(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     summary = db.Column(db.String(50000))
     comments_id = db.Column(db.Integer, db.ForeignKey('comments.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 class LabeledComments(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     sentiment = db.Column(db.String(150))
     comments_id = db.Column(db.Integer, db.ForeignKey('comments.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    frequentWords = db.relationship('FrequentWords')
+    sentimentCounter = db.relationship('SentimentCounter')
 
 class FrequentWords(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -30,6 +37,7 @@ class FrequentWords(db.Model):
     count = db.Column(db.Integer)
     sentiment = db.Column(db.String(150))
     labeledComments_id = db.Column(db.Integer, db.ForeignKey('labeled_comments.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 class SentimentCounter(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -37,6 +45,7 @@ class SentimentCounter(db.Model):
     negative = db.Column(db.Integer)
     neutral = db.Column(db.Integer)
     labeledComments_id = db.Column(db.Integer, db.ForeignKey('labeled_comments.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -46,3 +55,7 @@ class User(db.Model, UserMixin):
     profile_pic = db.Column(db.String(150), default='default.jpg')
     created_at = db.Column(db.DateTime(timezone=True), default=func.now())
     youtube_url = db.relationship('YoutubeUrl')
+    comments = db.relationship('Comments')
+    summarizedComments = db.relationship('SummarizedComments')
+    labeledComments = db.relationship('LabeledComments')
+    frequentWords = db.relationship('FrequentWords')
